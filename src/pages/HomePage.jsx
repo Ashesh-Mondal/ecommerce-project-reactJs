b8ee21469a6268d51679b4bb7bd22521fbe7ccb5
ashesh-mondal <asheshmondal84@gmail.com>
2025-12-08 21:32:24 +0530

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import Product from "../components/Product";
 import "./HomePage.css";
 
 function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
