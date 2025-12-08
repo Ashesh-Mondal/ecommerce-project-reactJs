d042021f27fe4a0d7db912d8df899c620e65f57e
ashesh-mondal <asheshmondal84@gmail.com>
2025-12-08 13:18:39 +0530

    Split checkout header into component and used Link

--- src/pages/Checkout/CheckoutPage.jsx
+++ src/pages/Checkout/CheckoutPage.jsx
@@ -1,5 +1,5 @@
+import { CheckoutHeader } from "./CheckoutHeader";
 import "./CheckoutPage.css";
-import "./checkout-header.css";
 
 function CheckoutPage() {
   return (
@@ -7,28 +7,7 @@ function CheckoutPage() {
       {/* Since React shares only a single HTML page i.e the / therefore the title of every single page is given in the top and the title from the index page is removed. */}
       <title>Checkout</title>
 
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <a className="return-to-home-link" href="/">
-              3 items
-            </a>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeader />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
